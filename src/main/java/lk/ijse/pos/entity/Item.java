e4c17bb532ea926ac284fba1959e6c980b3adda9
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-12 02:43:41 +0530

    add item entity

---
+++ src/main/java/lk/ijse/pos/entity/Item.java
@@ -0,0 +1,32 @@
+package lk.ijse.pos.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "itemId")
+    private Long itemId;
+
+    @Column(name = "itemName", length = 50, nullable = false)
+    private String itemName;
+
+    @Column(name = "qtyOnHand", nullable = false)
+    private double unitPrice;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "itemCategory",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    List<Item> items;
+}
--- src/main/java/lk/ijse/pos/entity/ItemCategory.java
+++ src/main/java/lk/ijse/pos/entity/ItemCategory.java
@@ -1,15 +1,27 @@
 package lk.ijse.pos.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class ItemCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "catId")
     private Long categoryId;
+
     @Column(name = "categoryName", length = 50, nullable = false)
     private String categoryName;
+
+    @ManyToOne
+    @JoinColumn(name = "itemId",nullable = false)
+    private ItemCategory itemCategory;
+
 }
