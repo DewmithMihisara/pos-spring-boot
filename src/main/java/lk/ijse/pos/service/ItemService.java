dc11c8333b3d59088a91264e6e36496f15ff1804
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-30 00:54:07 +0530

    done ItemService

--- src/main/java/lk/ijse/pos/service/ItemService.java
+++ src/main/java/lk/ijse/pos/service/ItemService.java
@@ -1,4 +1,14 @@
 package lk.ijse.pos.service;
 
+import lk.ijse.pos.dto.ItemDTO;
+import lk.ijse.pos.entity.Item;
+
+import java.util.List;
+
 public interface ItemService {
+    List<Item> getAllItems();
+    Item getItemById(Long id);
+    Item saveItem(ItemDTO itemDTO);
+    Item updateItem(ItemDTO itemDTO);
+    Item deleteItem(Long id);
 }
