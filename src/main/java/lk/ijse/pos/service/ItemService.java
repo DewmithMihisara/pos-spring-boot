8038656821934e5013f5cdf1398e88336a5868fb
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-30 00:54:22 +0530

    done ItemService

--- src/main/java/lk/ijse/pos/service/ItemService.java
+++ src/main/java/lk/ijse/pos/service/ItemService.java
@@ -2,9 +2,10 @@ package lk.ijse.pos.service;
 
 import lk.ijse.pos.dto.ItemDTO;
 import lk.ijse.pos.entity.Item;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface ItemService {
     List<Item> getAllItems();
     Item getItemById(Long id);
