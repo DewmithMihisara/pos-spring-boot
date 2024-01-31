9fa4ba5a4b547c80ec132cd8bfaa0541a212f2a8
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-31 21:43:03 +0530

    item entity add

--- src/main/java/lk/ijse/pos/entity/Item.java
+++ src/main/java/lk/ijse/pos/entity/Item.java
@@ -1,5 +1,6 @@
 package lk.ijse.pos.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -17,6 +20,8 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long code;
+    @Column(name = "name", nullable = false)
+    private String name;
     @Column(name = "unitPrice", nullable = false)
     private double unitPrice;
     @Column(name = "qtyOnHand", nullable = false)
@@ -24,4 +29,10 @@ public class Item {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private ItemCategory category;
+
+    @JsonIgnore
+    @ManyToMany(
+            mappedBy = "items"
+    )
+    private Set<Orders> orders = new HashSet<>();
 }
