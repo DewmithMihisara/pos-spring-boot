86b7a75d4c3811b17e970223a5ceb132bfe424f5
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-31 20:47:56 +0530

    order entity add

--- src/main/java/lk/ijse/pos/entity/Orders.java
+++ src/main/java/lk/ijse/pos/entity/Orders.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -19,11 +21,25 @@ public class Orders {
     @Column(name = "orderId")
     private Long orderId;
 
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(name ="date")
-    private Date date;
+    private LocalDateTime date;
+
+    @Column(name = "qty")
+    private Integer qty;
+
+    @Column(nullable = false)
+    private Double total;
 
     @ManyToOne
     @JoinColumn(name = "customerId",nullable = false)
     private Customer customer;
 
+    @ManyToMany
+    @JoinTable(
+            name = "order_item",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private Set<Item> items =new HashSet<>();
 }
