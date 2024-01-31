b7e72c5b77ce738f744899cc1d8a512865fed0f3
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-31 21:44:34 +0530

    customer dto add

--- src/main/java/lk/ijse/pos/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/pos/dto/CustomerDTO.java
@@ -1,11 +1,9 @@
 package lk.ijse.pos.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 public class CustomerDTO {
     private Long id;
