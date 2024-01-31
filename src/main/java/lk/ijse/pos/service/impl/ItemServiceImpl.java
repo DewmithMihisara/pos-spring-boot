e1a1bd01c1cc8cb285091d7bc42ca6a3ef606de9
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-31 22:50:28 +0530

    item service add

--- src/main/java/lk/ijse/pos/service/impl/ItemServiceImpl.java
+++ src/main/java/lk/ijse/pos/service/impl/ItemServiceImpl.java
@@ -2,15 +2,22 @@ package lk.ijse.pos.service.impl;
 
 import lk.ijse.pos.dto.ItemDTO;
 import lk.ijse.pos.entity.Item;
+import lk.ijse.pos.entity.ItemCategory;
+import lk.ijse.pos.repository.CategoryRepository;
 import lk.ijse.pos.repository.ItemRepository;
 import lk.ijse.pos.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ItemServiceImpl implements ItemService {
     @Autowired
     private ItemRepository itemRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public List<Item> getAllItems() {
         return itemRepository.findAll();
@@ -23,27 +30,30 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public Item saveItem(ItemDTO itemDTO) {
-        Item item=itemRepository.findById(itemDTO.getId()).orElse(null);
+        ItemCategory itemCategory = categoryRepository.findById(itemDTO.getCat_id()).orElse(null);
 
-        if(item==null){
-            Item newItem=new Item();
-            newItem.setCode(itemDTO.getId());
-            newItem.setUnitPrice(itemDTO.getUnitPrice());
-            newItem.setQtyOnHand(itemDTO.getQtyOnHand());
-            return itemRepository.save(newItem);
-        }else {
+        if (itemCategory != null) {
+            Item item = new Item();
+            item.setName(itemDTO.getName());
+            item.setCode(itemDTO.getId());
+            item.setUnitPrice(itemDTO.getUnitPrice());
+            item.setQtyOnHand(itemDTO.getQtyOnHand());
+            item.setCategory(itemCategory);
+            return itemRepository.save(item);
+        } else {
             return null;
         }
     }
 
     @Override
     public Item updateItem(ItemDTO itemDTO) {
-        Item item=itemRepository.findById(itemDTO.getId()).orElse(null);
-        if(item!=null){
+        Item item = itemRepository.findById(itemDTO.getId()).orElse(null);
+        if (item != null) {
+            item.setName(itemDTO.getName());
             item.setUnitPrice(itemDTO.getUnitPrice());
             item.setQtyOnHand(itemDTO.getQtyOnHand());
             return itemRepository.save(item);
-        }else {
+        } else {
             return null;
         }
     }
@@ -55,6 +65,17 @@ public class ItemServiceImpl implements ItemService {
             itemRepository.delete(item);
             return item;
         }else {
+            return null;
+        }
+    }
+
+    @Override
+    public List<Item> getItemByCategory(Long id) {
+        ItemCategory category = categoryRepository.findById(id).orElse(null);
+
+        if(category != null) {
+            return itemRepository.findProductsByCategory(category);
+        } else {
             return null;
         }
     }
