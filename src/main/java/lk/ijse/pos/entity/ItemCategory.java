764e7c5903962be53c7b19de0bd66faafc5ad624
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-31 21:43:17 +0530

    item cat entity add

--- src/main/java/lk/ijse/pos/entity/ItemCategory.java
+++ src/main/java/lk/ijse/pos/entity/ItemCategory.java
@@ -2,9 +2,14 @@ package lk.ijse.pos.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class ItemCategory {
     @Id
